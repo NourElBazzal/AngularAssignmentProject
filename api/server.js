edfc0d1d1bb455bff341452932eee9de5038d0ae
fubsbph <alloexpert12072015@gmail.com>
2025-06-02 11:26:08 +0200

    hhh

--- api/server.js
+++ api/server.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose');
 const cors = require('cors'); 
 const path = require('path');
 require('dotenv').config();
+const port = process.env.PORT || 8010;
+app.listen(port, () => {
+  console.log(`Serveur lancé sur http://localhost:${port}`);
+});
 
 mongoose.Promise = global.Promise;
 
